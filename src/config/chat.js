9a25d58a1b9c1ccdbfcfccad9430fe1ee00957df
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2022-05-16 12:58:45 +0200

    Chat with client-id

--- src/config/chat.js
+++ src/config/chat.js
@@ -1,11 +1,13 @@
 const BAS_API_URL = 'https://chabloz.eu/ws/chat/';
 
-export const apiLogin = BAS_API_URL + 'user/login?user=';
+export const clientId = (new Date()).getTime();
 
-export const apiLogout = BAS_API_URL + 'user/logout';
+export const apiLogin = BAS_API_URL + `user/login?cid=${clientId}&user=`;
 
-export const apiOnline = BAS_API_URL + 'user/online';
+export const apiLogout = BAS_API_URL + `user/logout?cid=${clientId}`;
 
-export const apiMsgAdd = BAS_API_URL + 'msg/add?msg=';
+export const apiOnline = BAS_API_URL + `user/online?cid=${clientId}`;
 
-export const apiMsgGet = BAS_API_URL + 'msg/get';
+export const apiMsgAdd = BAS_API_URL + `msg/add?cid=${clientId}&msg=`;
+
+export const apiMsgGet = BAS_API_URL + `msg/get?cid=${clientId}`;
